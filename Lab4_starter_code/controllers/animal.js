cafb7fca8847748636d47153e8f86e5908539f49
adaml09 <adamleblancleafs@gmail.com>
2024-04-18 20:03:16 -0400

    Added Controller function for entering an animal

---
+++ Lab4_starter_code/controllers/animal.js
@@ -0,0 +1,34 @@
+const { request } = require("express");
+const Animal = require("../model/Animal");
+
+function displayHomePage(req, res){
+    res.render("main.ejs")
+}
+
+async function enterAnimal(req, res){
+    const formData = req.body
+
+    //create our new database pokemon
+    try{
+        await Animal.create({
+            Zoo: formData.homeZoo,
+            ScientificName: formData.scientificName,
+            CommonName: formData.commonName,
+            GivenName: formData.givenName,
+            Gender: formData.gender, 
+            DateOfBirth: formData.dateOfBirth,
+            Age: formData.age,
+            isTransportable: isTransportable, 
+        })
+    }catch (err){
+        console.log(`ERROR in creating pokemon ${formData.commonNameName}`)
+    }
+    res.redirect("/");
+}
+
+
+
+module.exports = {
+    displayHomePage,
+    enterAnimal,
+}
